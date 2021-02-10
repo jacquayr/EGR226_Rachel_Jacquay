2bc5252a6e5e5f683a2a73a7affff98a947e8cb9
Rachel Jacquay <jacquayr@mail.gvsu.edu>
2021-02-10 08:18:33 -0500

    Lab 3 - Code Composer Files

---
+++ Lab 3/EGR226_Lab3_Part3/main.c
@@ -0,0 +1,22 @@
+#include "msp.h"
+
+/*
+ * main.c
+ */
+
+void main(void)
+{
+    // stop watchdog timer (WDT is used to break you out of unwanted loops, more on this later in the semester)
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+
+	// configure GPIO for output on P1.0 LED1 on MSP432 Launchpad
+	P1->DIR = BIT0;
+
+	// temporary variable for loop-maintenance
+	int i;
+	int change;
+	while (1) {
+	    P1->OUT ^= BIT0;     // toggle LED status
+	    for (i = change; i > 0; i--);
+	}
+}
