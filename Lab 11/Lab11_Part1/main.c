16db0256eff4ed9014e9fc7298d804870006e3aa
Rachel Jacquay <jacquayr@mail.gvsu.edu>
2021-04-04 16:19:20 -0400

    Lab 11 Updates

--- Lab 11/Lab11_Part1/main.c
+++ Lab 11/Lab11_Part1/main.c
@@ -4,34 +4,42 @@
  * Date:            04/04/2021
  * Project:         Lab 11 Parts 1 + 2
  * File:            main.c
- * Description:
+ * Description:     This lab works with IR emitter and receiver to
 -----------------------------------------------------------------------------------------*/
 
 // libraries
 #include "msp.h"
-#include <stdio.h>
-
-// function prototypes
-void emitter_init(void);
-void receive_init(void);
-void LED_init(void);
-void TA0_N_IRQHandler(void);
+#include "stdio.h"
 
 // globals
 uint8_t detect10Hz;
-volatile uint16_t lastedge, currentedge, period;
+volatile uint16_t lastedge, currentedge, period, flag;
+
+// function prototypes
+void emitter_init(void);
+void receiver_init(void);
+void LED_init(void);
+void TA2_N_IRQHandler(void);
 
 void main(void) {
+    __disable_irq();                                // disable irq
     WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
     emitter_init();                                 // initializations
-    receive_init();
+    receiver_init();
     LED_init();
 
-    NVIC_EnableIRQ(TA0_N_IRQn);     // NVIC call
-    __enable_irq();                    // enable irq
+    NVIC_EnableIRQ(TA2_N_IRQn);
+    __enable_irq();                 // enable irq
 
     while(1) {
+        flag = 1;
+        __delay_cycles(1000000);
 
+        if (flag == 1) {
+            P1->OUT &= ~BIT0;
+            flag = 0;
+            __delay_cycles(100);
+        }
     }
 }
 
@@ -59,25 +67,27 @@ void emitter_init(void) {
 }
 
 /*--------------------------------------------------------------
- * Function:
+ * Function:        receiever_init
  *
- * Description:
+ * Description:     This function initializes the receiver LED.
  *
  * Inputs:          none
  *
  * Outputs:         none
  *-------------------------------------------------------------*/
-void receive_init(void) {
-    P6->SEL0 |= BIT6;
-    P6->SEL1 &= ~BIT6;
-    P6->DIR &= ~BIT6;
+void receiver_init(void) {
+    P5->SEL0 |= BIT6;
+    P5->SEL1 &= ~BIT6;
+    P5->DIR &= ~BIT6;
 
-    TIMER_A0->CTL |= 0x0214;
-    TIMER_A0->CCTL[2] = 0x4910;
+    TIMER_A2->CTL |= 0x0214;
+    TIMER_A2->CCR[0] = 65535;
+    TIMER_A2->CCTL[1] = TIMER_A_CCTLN_OUTMOD_7;
+    TIMER_A2->CCTL[1] = 0x4910;
 }
 
 /*--------------------------------------------------------------
- * Function:
+ * Function:        TA0_N_IRQHandler
  *
  * Description:
  *
@@ -86,16 +96,13 @@ void receive_init(void) {
  * Outputs:         none
  *-------------------------------------------------------------*/
 void LED_init(void) {
-    P2->SEL0 &= ~BIT0;
-    P2->SEL1 &= ~BIT0;
-    P2->DIR |= BIT0;
-    P2->SEL0 |= BIT0;
-
-    TIMER32_2->CONTROL = 0b11100011;
+    P1->SEL0 &= ~BIT0;
+    P1->SEL1 &= ~BIT0;
+    P1->DIR |= BIT0;
 }
 
 /*--------------------------------------------------------------
- * Function:
+ * Function:        TA0_N_IRQHandler
  *
  * Description:
  *
@@ -103,19 +110,22 @@ void LED_init(void) {
  *
  * Outputs:         none
  *-------------------------------------------------------------*/
-void TA0_N_IRQHandler(void) {
-    TIMERA0->CCTL[2] &= ~1;
-    currentedge = TIMERA0_->CCR[2];
+void TA2_N_IRQHandler(void) {
+    TIMER_A2->CCTL[1] &= ~1;
+    currentedge = TIMER_A2->CCR[1];
     period = currentedge - lastedge;
     lastedge = currentedge;
 
-    if ((39635 < period) && (period < 39375)) {
-        detect10Hz = 1;
+    flag = 0;
+
+    if ((35635 < period) && (period < 39375)) {
+        __delay_cycles(500);
+        if ((35635 < period) && (period < 39375)) {
+            detect10Hz = 1;
+            P1->OUT |= BIT0;
+            __delay_cycles(100000);
+        }
     }
 
-    if (detect10Hz == 1) {
-        TIMER32_2->INTCLR = 1;
-        P2->OUT ^= BIT0;
-        TIMER32_2->LOAD = 9000000;
-    }
+    TIMER_A0->CCTL[1] &= ~(TIMER_A_CCTLN_CCIFG);
 }
